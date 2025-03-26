fef1af6ee33b0f9411864949bd49cbab0a02e7b3
Jaap Weijland <jaapweijland@gmail.com>
2025-03-26 11:56:10 +0100

    fix useserachparams

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+// This file is intentionally left blank
+// It is needed to allow top level use of useSearchParams on page.tsx
+export default function Loading() {
+  return <></>;
+}
